cac068771352988bf7816fbf7e685e605ccdefc2
Kyuseishu <kyuseishu1593@gmail.com>
2024-08-11 18:33:06 +0700

    feature: field activity

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     // #region handle hover coreValueAndCommitmentHeadingItem
-    const coreValueAndCommitmentHeadingItem = document.querySelectorAll('.core-value-and-commitment__body__content__list__item__heading')
-    coreValueAndCommitmentHeadingItem.forEach(element => {
+    const coreValueAndCommitmentHeadingItems = document.querySelectorAll('.core-value-and-commitment__body__content__list__item__heading')
+    coreValueAndCommitmentHeadingItems.forEach(element => {
         const parent = element.parentElement.parentElement
         if (!parent) return
         const coreValueAndCommitmentRubyHover = parent.querySelector('.core-value-and-commitment__body__content__list__item .hover')
         const coreValueAndCommitmentRubyUnHover = parent.querySelector('.core-value-and-commitment__body__content__list__item .un-hover')
-        handleEventHover(element, coreValueAndCommitmentRubyHover, coreValueAndCommitmentRubyUnHover)
+        handleEventHoverCommitmentHeadingItem(element, coreValueAndCommitmentRubyHover, coreValueAndCommitmentRubyUnHover)
     })
 
-    function handleEventHover(target, hover, unHover) {
+    function handleEventHoverCommitmentHeadingItem(target, hover, unHover) {
         target.addEventListener('mouseover', () => {
             hover.style.display = 'block'
             unHover.style.display = 'none'
@@ -20,6 +20,30 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
     // #endregion handle hover coreValueAndCommitmentHeadingItem
+
+    // #region handle hover field activity item
+    const fieldActivityItems = document.querySelectorAll('.field-activity__body__list__item')
+    fieldActivityItems.forEach(fieldActivityItem => {
+        const substractBg = fieldActivityItem.querySelector('.substract-bg')
+        const over = fieldActivityItem.querySelector('.over')
+        const sub = over.querySelector('.substract-bg')
+        if (!substractBg) return
+        const bgShow = substractBg.dataset.img
+        const coverColor = substractBg.dataset.cover
+
+        // fieldActivityItem.style.background = `url('${bgShow}') center/cover, #383838`
+        console.log(bgShow)
+        sub.style.backgroundImage = `url('${bgShow}')`
+        fieldActivityItem.addEventListener('mouseover', () => {
+            over.style.display = 'block'
+            // over.style.backgroundImage = bgShow
+            // console.log(over.style.backgroundImage)
+        })
+        fieldActivityItem.addEventListener('mouseout', () => {
+            over.style.display = 'none'
+        })
+    })
+    // #endregion handle hover field activity item
 
     // const fieldActivityList = document.querySelector('.field-activity__list')
 
