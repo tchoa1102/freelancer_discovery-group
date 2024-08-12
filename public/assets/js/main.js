7783a2474f17045f96203bae327dc6c5a98b27f1
Kyuseishu <kyuseishu1593@gmail.com>
2024-08-12 23:34:16 +0700

    fix: blog

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -25,22 +25,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const fieldActivityItems = document.querySelectorAll('.field-activity__body__list__item')
     fieldActivityItems.forEach(fieldActivityItem => {
         const substractBg = fieldActivityItem.querySelector('.substract-bg')
-        const over = fieldActivityItem.querySelector('.over')
-        const sub = over.querySelector('.substract-bg')
+        // const over = fieldActivityItem.querySelector('.over')
+        // const sub = over.querySelector('.substract-bg')
         if (!substractBg) return
         const bgShow = substractBg.dataset.img
         const coverColor = substractBg.dataset.cover
+        const bgColor = substractBg.dataset.color
 
         // fieldActivityItem.style.background = `url('${bgShow}') center/cover, #383838`
         // console.log(bgShow)
-        sub.style.backgroundImage = `url('${bgShow}')`
+        // sub.style.backgroundImage = `url('${bgShow}')`
         fieldActivityItem.addEventListener('mouseover', () => {
-            over.style.display = 'block'
+            // substractBg.style.backgroundBlendMode = 'lighten'
+            // substractBg.style.backgroundColor = '#383838b3'
+            // const bg = `background: url(./public/assets/img/field-activity/3.png) center/cover, #383838b3`
+            // substractBg.style.background = "background: url(./public/assets/img/field-activity/3.png) center/cover, #383838b3"
+            // substractBg.style.background = "#f2f8fd"
+            // over.style.display = 'block'
             // over.style.backgroundImage = bgShow
             // console.log(over.style.backgroundImage)
         })
         fieldActivityItem.addEventListener('mouseout', () => {
-            over.style.display = 'none'
+            // substractBg.style.backgroundBlendMode = 'lighten'
+            // substractBg.style.backgroundColor = '#f2f8fd'
+            // over.style.display = 'none'
         })
     })
     // #endregion handle hover field activity item
@@ -57,4 +65,42 @@ document.addEventListener('DOMContentLoaded', () => {
         e.preventDefault()
         navMobileModal.style.display = 'none'
     })
+
+    function checkScrollPosition() {
+        var w = window.innerWidth;
+        var buttons = document.getElementById("float-buttons");
+        var icons = document.getElementById("float-icons");
+        var scrollTopbtn = document.getElementById("scroll-top-button");
+        if (w > 769) {
+          if (window.scrollY <= 500) {
+            buttons.style.display = "none";
+            icons.style.display = "block";
+          } else {
+            icons.style.display = "none";
+            buttons.style.display = "flex";
+          }
+      
+          if(window.scrollY >= 4000) {
+            scrollTopbtn.style.display = "block";
+          } else {
+            scrollTopbtn.style.display = "none";
+          }
+        } else {
+          buttons.style.display = "none";
+          icons.style.display = "none";
+        }
+      }
+      
+      document.getElementById('scroll-top-button').addEventListener('click', function() {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      });
+      
+      // Check scroll position on page load
+      window.onload = checkScrollPosition;
+      
+      // Check scroll position on scroll
+      window.onscroll = checkScrollPosition;
 })
